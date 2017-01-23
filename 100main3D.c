79c8b60e612a6d29dcaafedf4fe7ebb5bb885b76
connellyj <juliakconnelly@gmail.com>
2017-01-22 22:44:48 -0600

    3D ANIMATION

---
+++ 100main3D.c
@@ -0,0 +1,196 @@
+/* Julia Connelly and Kerim Celik, 01/20/2017 */
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <math.h>
+#include <stdlib.h>
+#include "000pixel.h"
+#include "100vector.c"
+#include "100matrix.c"
+#include "040texture.c"
+#include "090renderer.c"
+
+#define renVERTNUMBOUND 50
+#define renVARYDIMBOUND 16
+
+#define renATTRX 0
+#define renATTRY 1
+#define renATTRZ 2
+#define renATTRS 3
+#define renATTRT 4
+#define renATTRN 5
+#define renATTRO 6
+#define renATTRP 7
+
+#define renVARYX 0
+#define renVARYY 1
+#define renVARYS 2
+#define renVARYT 3
+
+#define renUNIFR 0
+#define renUNIFG 1
+#define renUNIFB 2
+#define renUNIFTHETA 3
+#define renUNIFPHI 4
+#define renUNIFALPHA 5
+#define renUNIFTRANSX 6
+#define renUNIFTRANSY 7
+#define renUNIFTRANSZ 8
+#define renUNIFM 9
+
+#define renTEXR 0
+#define renTEXG 1
+#define renTEXB 2
+
+double animRot = 0;
+
+/* Sets rgb, based on the other parameters, which are unaltered. vary is an 
+interpolated varying vector. */
+void colorPixel(renRenderer *ren, double unif[], texTexture *tex[], 
+        double vary[], double rgb[]) {
+    texSample(tex[0], vary[renVARYS], vary[renVARYT]);
+    rgb[0] = tex[0]->sample[renTEXR];
+    rgb[1] = tex[0]->sample[renTEXG];
+    rgb[2] = tex[0]->sample[renTEXB];
+}
+
+/* Writes the vary vector, based on the other parameters. */
+void transformVertex(renRenderer *ren, double unif[], double attr[], 
+        double vary[]) {
+    double vary4[4] = {attr[renATTRX], attr[renATTRY], attr[renATTRZ], 1};
+    mat441Multiply((double(*)[4])(&unif[renUNIFM]), vary4, vary4);
+    vary[renVARYX] = vary4[renVARYX]; vary[renVARYY] = vary4[renVARYY];
+    vary[renVARYS] = attr[renATTRS]; vary[renVARYT] = attr[renATTRT];
+    unif[renUNIFALPHA] += animRot;
+}
+
+/* If unifParent is NULL, then sets the uniform matrix to the 
+rotation-translation M described by the other uniforms. If unifParent is not 
+NULL, but instead contains a rotation-translation P, then sets the uniform 
+matrix to the matrix product P * M. */
+void updateUniform(renRenderer *ren, double unif[], double unifParent[]) {
+    double axis[3];
+    vec3Spherical(1.0, unif[renUNIFPHI], unif[renUNIFTHETA], axis);
+    double rot[3][3];
+    mat33AngleAxisRotation(unif[renUNIFALPHA], axis, rot);
+    double trans[3] = {unif[renUNIFTRANSX], unif[renUNIFTRANSY], unif[renUNIFTRANSZ]};
+    if (unifParent == NULL) {
+        /* &unif[renUNIFM] is an array containing nine numbers */
+        mat44Isometry(rot, trans, (double(*)[4])(&unif[renUNIFM]));
+    }else {
+        double m[4][4];
+        mat44Isometry(rot, trans, m);
+        mat444Multiply((double(*)[4])(&unifParent[renUNIFM]), m, 
+            (double(*)[4])(&unif[renUNIFM]));
+    }
+}
+
+#include "100triangle.c"
+#include "100mesh.c"
+#include "090scene.c"
+
+texTexture *t00[1], *t01[1];
+texTexture t0, t1;
+renRenderer r;
+meshMesh m0, m1;
+sceneNode top, mid1;
+int animate = 0;
+
+void initMesh(void) {
+    meshInitializeSphere(&m0, 50, 40, 20);
+    meshInitializeBox(&m1, 0, 100, 0, 100, 0, 100);
+}
+
+void initRenderer(void) {
+    r.unifDim = 3 + 3 + 3 + 16; r.texNum = 2; r.attrDim = 8; r.varyDim = 4;
+    r.colorPixel = colorPixel;
+    r.transformVertex = transformVertex;
+    r.updateUniform = updateUniform;
+}
+
+int initTextures(void) {
+    if(texInitializeFile(&t0, "cat.jpg") != 0){
+        return 1;
+    }
+    if(texInitializeFile(&t1, "test.jpg") != 0){
+        return 1;
+    }
+    t00[0] = &t0; t01[0] = &t1;
+    texSetFiltering(&t0, 0); texSetFiltering(&t1, 0);
+    return 0;
+}
+
+void handleMouseDown(int button, int shiftIsDown, int controlIsDown,
+		int altOptionIsDown, int superCommandIsDown) {
+	animate = 1;
+}
+
+/* Similarly, the following callbacks handle some mouse interactions. */
+void handleMouseUp(int button, int shiftIsDown, int controlIsDown,
+		int altOptionIsDown, int superCommandIsDown) {
+	animate = 0;
+}
+
+void draw() {
+    pixClearRGB(0.0, 0.0, 0.0);
+    sceneRender(&top, &r, NULL);
+}
+
+/* This callback is called once per animation frame. As parameters it receives 
+the time for the current frame and the time for the previous frame. Both times 
+are measured in seconds since some distant past time. */ 
+void handleTimeStep(double oldTime, double newTime) {
+    if(animate == 1) {
+        animRot = 0.001 * (newTime - oldTime);
+        draw();
+    }
+}
+
+int main(void) {
+    /* initialize the window */
+    if (pixInitialize(512, 512, "Pixel Graphics") != 0)
+		return 1;
+	else {
+        /* reset the screen to black */
+		pixClearRGB(0.0, 0.0, 0.0);
+        
+        /* Initialize all components needed to create the scene */
+        double unif0[25] = {1, 1, 1,
+                            0.78, 0.78, 0,
+                            250, 250, 0,
+                            1, 0, 0, 0,
+                            0, 1, 0, 0,
+                            0, 0, 1, 0,
+                            0, 0, 0, 1};
+        double unif1[25] = {1, 1, 1,
+                            0, 0, 0,
+                            0, 0, 0,
+                            1, 0, 0, 0,
+                            0, 1, 0, 0,
+                            0, 0, 1, 0,
+                            0, 0, 0, 1};
+        initMesh();
+        initRenderer();
+        if(initTextures() == 1) return 1;
+        
+        /* Initialize the scene */
+        sceneInitialize(&mid1, &r, unif1, t01, &m1, NULL, NULL);
+        sceneInitialize(&top, &r, unif0, t00, &m0, &mid1, NULL);
+        
+        /* Render the scene */
+        sceneRender(&top, &r, NULL);
+        
+        pixSetMouseDownHandler(handleMouseDown);
+        pixSetTimeStepHandler(handleTimeStep);
+        pixSetMouseUpHandler(handleMouseUp);
+        
+        /* start the event loop */
+		pixRun();
+        
+        /* Clean up */
+        texDestroy(&t0); texDestroy(&t1);
+        meshDestroy(&m0); meshDestroy(&m1);
+        sceneDestroyRecursively(&top);
+		return 0;
+	}
+}
